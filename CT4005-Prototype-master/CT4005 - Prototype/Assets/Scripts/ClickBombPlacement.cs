7946d0b369c5c30d89dc3e4f3c06bb3a00f0f81d
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2020-01-28 13:52:00 +0000

    Paint WIP

---
+++ CT4005-Prototype-master/CT4005 - Prototype/Assets/Scripts/ClickBombPlacement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBombPlacement : MonoBehaviour
+{
+    public GameObject bombPrefab;
+
+    private float timerBomb;
+    // Update is called once per frame
+    void Update()
+    {
+        timerBomb -= Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && timerBomb < 0)
+        {
+            GameObject bombInstance = Instantiate(bombPrefab);
+
+            bombInstance.transform.position = transform.position;
+
+            bombInstance.transform.localRotation = Quaternion.identity;
+
+            //bombInstance.transform.localScale = Vector3.one;
+
+            //Vector3 trueScale = bombInstance.transform.lossyScale;
+
+            //bombInstance.transform.localScale = new Vector3(1 / trueScale.x, 1 / trueScale.y, 1 / trueScale.z);
+
+            bombInstance.GetComponent<bomb>().LightFuse();
+            timerBomb = 3f;
+        }
+    }
+    private void Start()
+    {
+        timerBomb = 0f;
+    }
+}
---
+++ CT4005-Prototype-master/CT4005 - Prototype/Assets/Scripts/bomb.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+
+    public float explosionForce = 10;
+    public float explosionRadius = 5;
+    public float fuseCountdown = 3;
+
+
+    private bool litFuse = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (litFuse)
+        {
+            fuseCountdown -= Time.deltaTime;
+
+            if(fuseCountdown <= 0)
+            {
+                SelfDestruct();
+            }
+        }
+    }
+    public void LightFuse()
+    {
+        litFuse = true;
+    }
+
+    private void SelfDestruct()
+    {
+        Collider[]colliders = Physics.OverlapSphere(transform.position, 5f);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody r = collider.GetComponent<Rigidbody>();
+            if (r != null)
+            {
+                r.AddExplosionForce(explosionForce,transform.position, explosionRadius, 0, ForceMode.Impulse);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
+}
